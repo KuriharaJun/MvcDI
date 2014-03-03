1694d9c40be7e085114ff55ee474202ae7b2ab90
Kurihara <kurihara.033@t-ns.co.jp>
2014-03-03 19:48:06 +0900

    メール入力チェック属性修正

--- TestApplication/MvcDI/MailAttribute.cs
+++ TestApplication/MvcDI/MailAttribute.cs
@@ -8,9 +8,9 @@ namespace MvcDI
     public class MailAttribute : RegularExpressionAttribute
     {
         public MailAttribute()
-            : base("^[a-z0-9~!$%^&*_=+}{\'?]+(.[a-z0-9~!$%^&*_=+}{\'?]+)*@"
-            + "([a-z0-9_][a-z0-9_]*(.[a-z0-9_]+)*.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])"
-            + "|([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}))(:[0-9]{1,5})?")
+            : base("^[a-z0-9~!$%^&*_=+}{\'?]+(\\.[a-z0-9~!$%^&*_=+}{\'?]+)*@"
+            + "([a-z0-9_][a-z0-9_]*(\\.[a-z0-9_]+)*\\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])"
+            + "|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?")
         {
         }
     }
