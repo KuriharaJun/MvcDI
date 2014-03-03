75c3b424a0b8cf03a08a2016beb3da4c621326ec
Kurihara <kurihara.033@t-ns.co.jp>
2014-03-03 19:47:37 +0900

    時刻入力チェック属性追加
    Signed-off-by: Kurihara <kurihara.033@t-ns.co.jp>

---
+++ TestApplication/MvcDI/TelAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDI
+{
+    public class TelAttribute : RegularExpressionAttribute
+    {
+        public TelAttribute() : base("^\\d{2,4}-\\d{2,4}-\\d{4}$") { }
+    }
+}
