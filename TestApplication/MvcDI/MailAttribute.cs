70e262118156e7e613ea9af64f13a4883f3588f4
Kurihara <kurihara.033@t-ns.co.jp>
2014-03-03 19:07:30 +0900

    入力チェック属性の追加 ・メールアドレス ・時刻
    Signed-off-by: Kurihara <kurihara.033@t-ns.co.jp>

---
+++ TestApplication/MvcDI/MailAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDI
+{
+    /// <summary>
+    /// メール入力形式チェック属性
+    /// </summary>
+    public class MailAttribute : RegularExpressionAttribute
+    {
+        public MailAttribute()
+            : base("^[a-z0-9~!$%^&*_=+}{\'?]+(.[a-z0-9~!$%^&*_=+}{\'?]+)*@"
+            + "([a-z0-9_][a-z0-9_]*(.[a-z0-9_]+)*.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])"
+            + "|([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}))(:[0-9]{1,5})?")
+        {
+        }
+    }
+}
---
+++ TestApplication/MvcDI/TimeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDI
+{
+    /// <summary>
+    /// 時間入力形式チェック属性
+    /// </summary>
+    public class TimeAttribute : RegularExpressionAttribute
+    {
+        public TimeAttribute()
+            : base("^([0-1][0-9]|[2][0-3]):[0-5][0-9]$") { }
+    }
+}
