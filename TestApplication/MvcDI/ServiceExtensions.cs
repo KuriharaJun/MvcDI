f1e76eaea0c0f2b4b1e54e210fbc83de959a6aae
Kurihara <kurihara.033@t-ns.co.jp>
2014-03-04 12:01:26 +0900

    サービス拡張クラスの追加
    Signed-off-by: Kurihara <kurihara.033@t-ns.co.jp>

---
+++ TestApplication/MvcDI/ServiceExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System.Transactions;
+
+namespace MvcDI
+{
+    /// <summary>
+    /// ワーカーサービス実装インターフェース
+    /// </summary>
+    public interface IService
+    {
+        /// <summary>
+        /// トランザクション
+        /// </summary>
+        TransactionScope tx { get; set; }
+    }
+
+    /// <summary>
+    /// サービス拡張クラス
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// トランザクション開始処理
+        /// </summary>
+        /// <param name="service"></param>
+        public static void BeginTransaction(this IService service)
+        {
+            service.tx = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel= IsolationLevel.ReadCommitted,
+                Timeout = TransactionManager.DefaultTimeout
+            });
+        }
+
+        /// <summary>
+        /// コミット処理
+        /// </summary>
+        /// <param name="service"></param>
+        public static void Commit(this IService service)
+        {
+            try
+            {
+                service.tx.Complete();
+                EndTransaction(service);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// ロールバック処理
+        /// </summary>
+        /// <param name="service"></param>
+        public static void Roolback(this IService service)
+        {
+            try
+            {
+                EndTransaction(service);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// トランザクション終了処理
+        /// </summary>
+        /// <param name="service"></param>
+        public static void EndTransaction(this IService service)
+        {
+            try
+            {
+                service.tx.Dispose();
+            }
+            catch { }
+        }
+    }
+}
