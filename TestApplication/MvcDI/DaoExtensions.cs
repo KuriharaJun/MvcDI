ac4a0dae7a6594e2a392f5e954e6ac148f3895fe
Kurihara Jun <lh182051+src@gmail.com>
2014-03-11 01:06:31 +0900

    Daoクラス拡張処理作成

---
+++ TestApplication/MvcDI/DaoExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace MvcDI
+{
+    public static class DaoExtensions
+    {
+        public static SqlType ServerType { get; private set; }
+        public static DbCommand CommandTransaction(this IDao dao, DbTransaction tx)
+        {
+            switch (ServerType)
+            {
+                case SqlType.SqlServer:
+                    {
+                        var command = new SqlCommand();
+                        command.Connection = (SqlConnection)dao.con;
+                        command.Transaction = (SqlTransaction)tx;
+                        return command;
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        public static DbCommand CommandNonTransaction(this IDao dao)
+        {
+            switch (ServerType)
+            {
+                case SqlType.SqlServer:
+                    {
+                        var command = new SqlCommand();
+                        command.Connection = (SqlConnection) dao.con;
+                        return command;
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+    }
+}
